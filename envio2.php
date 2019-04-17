86f5c6c1ad9e9ad18d14d3a2c5c509e31b0c2098
Guiller <ghminvest@gmail.com>
2019-04-17 19:17:55 -0300

    Subindo os arquivos no github
    Adicionando a parte de código pronta.

---
+++ envio2.php
@@ -0,0 +1,51 @@
+<html>
+
+<head>
+	<title>Formulário de Envio de Contato.</title>
+
+</head>
+
+
+
+<body>
+
+<?php
+
+
+$name=$_POST ['Name'];
+$email=$_POST ['Email'];
+$phone=$_POST ['Phone'];
+$message=$_POST ['Message'];
+
+?>
+
+
+<?php
+
+$to = "guillerbrasilrj@gmail.com";
+$subject = "$email";
+$message = "Nome: $name   E-mail: $email    Telefone:$phone    
+ Mensagem:  $message";
+
+
+
+$header = "MIME-VERSION: 1.0\n";
+$header = "Content-Type: text/html; charset=iso-8859-1\n";
+$header ="From: $email\n";
+
+mail($to,$subject,$message,$header);
+echo "Mensagem enviada com sucesso!
+ Em Breve Responderemos para o Email!
+  Obrigado!"
+
+
+?>
+
+<br>
+<br>
+<br>
+<a href="index.html">VOLTAR</a>
+
+
+</body>
+</html>
